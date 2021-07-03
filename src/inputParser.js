247e9295237c09a20c02b90ab9b1c79cd4fd8cf9
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2021-07-03 14:21:59 +0545

    parser input received

--- src/inputParser.js
+++ src/inputParser.js
@@ -1,8 +1,59 @@
 
+function plotgraph() {
 
-function getdata()
-{
-    var str = document.getElementById("function").value;
+    funcinp = document.getElementById("function").value;
+    funcinp.trim();
+    if (funcinp.includes("sin")) {                                     
+        funcinp = funcinp.replaceAll("sin", "Math.sin");
+    };
+    if (funcinp.includes("cos")) {                                     
+        funcinp = funcinp.replaceAll("cos", "Math.cos")
+    };
+    if (funcinp.includes("tan")) {                                     
+        funcinp = funcinp.replaceAll("tan", "Math.tan")
+    };
+    if (funcinp.includes("^")) {                                       
+        funcinp = funcinp.replaceAll("^", "**")
+    };
+    if (funcinp.includes("sqrt")) {                                     
+        funcinp = funcinp.replaceAll("sqrt", "Math.sqrt")
+    };
+    if (funcinp.includes("pi")) {                                    
+        funcinp = funcinp.replaceAll("pi", "Math.PI")
+    };
+    if (funcinp.includes("e")) {                                       
+        funcinp = funcinp.replaceAll("e", "Math.E")
+    };
+    if (funcinp.includes("atan")) {                                       
+        funcinp = funcinp.replaceAll("atan", "Math.atan")
+    };
+    if (funcinp.includes("acos")) {                                       
+        funcinp = funcinp.replaceAll("acos", "Math.acos")
+    };
+    if (funcinp.includes("asin")) {                                       
+        funcinp = funcinp.replaceAll("asin", "Math.asin")
+    };
+    if (funcinp.includes("abs")) {                                       
+        funcinp = funcinp.replaceAll("abs", "Math.abs")
+    };
+    if (funcinp.includes("cbrt")) {                                      
+        funcinp = funcinp.replaceAll("cbrt", "Math.cbrt")
+    };
+    if (funcinp.includes("log")) {                                       
+        funcinp = funcinp.replaceAll("log", "Math.log")
+    };
+    if (funcinp.includes("ln")) {                                       
+        funcinp = funcinp.replaceAll("ln", "Math.log")
+    };
+    if (funcinp.includes("log10")) {                                      
+        funcinp = funcinp.replaceAll("log10", "Math.log10")
+    };
+    if (funcinp.includes("Sgn")) {                                      
+        funcinp = funcinp.replaceAll("Sgn", "Math.sign")
+    };
+
     
-    console.log(`input function is ${str}`);
+    console.log(eval(funcinp));
+  
 }
+
