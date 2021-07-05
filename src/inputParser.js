32f5e1bf0bd76b9f916edb79f6fba2447f141be8
Suyog-Dhakal <075bct092.suyog@pcampus.edu.np>
2021-07-05 12:50:58 +0545

    value controlled from slider

--- src/inputParser.js
+++ src/inputParser.js
@@ -1,59 +1,32 @@
 
+var str;
+var xslider;
+const domainstart = -10;
+const domainend = 10;
+
 function plotgraph() {
-
-    funcinp = document.getElementById("function").value;
-    funcinp.trim();
-    if (funcinp.includes("sin")) {                                     
-        funcinp = funcinp.replaceAll("sin", "Math.sin");
-    };
-    if (funcinp.includes("cos")) {                                     
-        funcinp = funcinp.replaceAll("cos", "Math.cos")
-    };
-    if (funcinp.includes("tan")) {                                     
-        funcinp = funcinp.replaceAll("tan", "Math.tan")
-    };
-    if (funcinp.includes("^")) {                                       
-        funcinp = funcinp.replaceAll("^", "**")
-    };
-    if (funcinp.includes("sqrt")) {                                     
-        funcinp = funcinp.replaceAll("sqrt", "Math.sqrt")
-    };
-    if (funcinp.includes("pi")) {                                    
-        funcinp = funcinp.replaceAll("pi", "Math.PI")
-    };
-    if (funcinp.includes("e")) {                                       
-        funcinp = funcinp.replaceAll("e", "Math.E")
-    };
-    if (funcinp.includes("atan")) {                                       
-        funcinp = funcinp.replaceAll("atan", "Math.atan")
-    };
-    if (funcinp.includes("acos")) {                                       
-        funcinp = funcinp.replaceAll("acos", "Math.acos")
-    };
-    if (funcinp.includes("asin")) {                                       
-        funcinp = funcinp.replaceAll("asin", "Math.asin")
-    };
-    if (funcinp.includes("abs")) {                                       
-        funcinp = funcinp.replaceAll("abs", "Math.abs")
-    };
-    if (funcinp.includes("cbrt")) {                                      
-        funcinp = funcinp.replaceAll("cbrt", "Math.cbrt")
-    };
-    if (funcinp.includes("log")) {                                       
-        funcinp = funcinp.replaceAll("log", "Math.log")
-    };
-    if (funcinp.includes("ln")) {                                       
-        funcinp = funcinp.replaceAll("ln", "Math.log")
-    };
-    if (funcinp.includes("log10")) {                                      
-        funcinp = funcinp.replaceAll("log10", "Math.log10")
-    };
-    if (funcinp.includes("Sgn")) {                                      
-        funcinp = funcinp.replaceAll("Sgn", "Math.sign")
-    };
-
     
-    console.log(eval(funcinp));
-  
+    evaluate();
+    console.log(result);  
+}
+
+function getSliderX() {
+    xslider = document.getElementById("rangeX").value;
+    //console.log(xslider);
+    return xslider;
+ }
+ 
+function evaluate() {
+    str = document.getElementById("function").value;
+    getSliderX();
+    let scope = {
+        x: xslider*10,
+        
+    }
+    const node = math.parse(str)
+    const code = node.compile()
+    result = code.evaluate(scope);
+    
+    return result;
 }
 
