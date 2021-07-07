8dceb8bde4d0472c9f3792094e2dce33a08c1ddb
santosh7117 <075bct081.santosh@pcampus.edu.np>
2021-07-07 06:58:20 +0545

    Separated private functions

--- src/samplesGenerator.js
+++ src/samplesGenerator.js
@@ -3,45 +3,56 @@ const SampleGenerator = function() {
   self.samples = null
   
   let minX, maxX, minY, maxY
-  let range_x, range_y
-  let input_str = ""
+  let range_x, range_y, old_range_x, old_range_y
+  let input_str = "", old_input_str = ""
   let x_samples = []
   let y_samples = []
   let z_samples = []
   let scope = {x: x_samples, y: y_samples}
     
-  const updateRanges = function() {
+  const _updateRanges = function() {
     range_x = document.getElementById("rangeX").value
     range_y = document.getElementById("rangeY").value
   }
 
-  const updateEqn = function() {
+  const _updateEqn = function() {
     input_str = document.getElementById("function").value
   }
 
-  const updateXYSamples = function() {    
+
+
+  const _updateXSamples = function() {
+    old_range_x = range_x
+
     minX = -10 * range_x
     maxX =  10 * range_x
+    
+    for (let x = minX; x <= maxX; x += SAMPLE_STEP_SIZE) {
+        x_samples.push(x)
+    }
+  }
+
+  const _updateYSamples = function() {
+    old_range_y = range_y
+    
     minY = -10 * range_y
     maxY =  10 * range_y
     
-    for (let x = minX, y = minY;
-      x <= maxX || y <= maxY; 
-      x += SAMPLE_STEP_SIZE, y += SAMPLE_STEP_SIZE
-    ) {
-        x_samples.push(x)
+    for (let y = minY; y <= maxY; y += SAMPLE_STEP_SIZE) {
         y_samples.push(y)
     }
   }
 
-  const updateZSamples = function() {
+  const _updateZSamples = function() {
+    old_input_str = input_str
+    
     if (input_str) {
       z_samples = math.evaluate(input_str, scope)
     }
   }
   
 
-  const zipSamples = function() {
+  const _zipSamples = function() {
     let zipped = []
     limit = range_x < range_y ? range_x : range_y
 
@@ -54,12 +65,14 @@ const SampleGenerator = function() {
   }
     
   self.updateSamples = function() {
-    updateRanges()
-    updateEqn()
-    updateXYSamples()
-    updateZSamples()
+    _updateRanges()
+    _updateEqn()
+    
+    if (old_range_x != range_x) _updateXSamples()
+    if (old_range_y != range_y) _updateYSamples()
+    if (old_input_str != input_str) _updateZSamples()
 
-    self.samples = new Float32Array([zipSamples()])
+    self.samples = new Float32Array([_zipSamples()])
   }
 
 }
